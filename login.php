8931fd28b8a5b2c010685a23190b85f3fc379426
Jason Leohandy <2511500035@mahasiswa.atmaluhur.ac.id>
2026-07-08 12:07:41 +0700

    main documment

--- login.php
+++ login.php
@@ -67,7 +67,7 @@ include "config/koneksi.php";
 <?php
 if (isset($_POST['login'])) {
     $Username = mysqli_real_escape_string($koneksi, $_POST['Username']);
-    $P  assword = $_POST['Password'];
+    $P password = $_POST['Password'];
 
     if (empty($Username) || empty($Password)) {
         echo '<script>alert("Username dan Password tidak boleh kosong!");</script>';
